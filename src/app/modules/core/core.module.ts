d9bea641152827bdc5857aaac43de3734d373508
Daniel Djordjevic <daniel.djordjevic@viacesi.fr>
2018-02-13 14:43:03 +0100

    Add best comment

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -14,6 +14,7 @@ import { UserService } from './services/user.service';
 export class CoreModule {
  }
 
+ // Uncomment this and add .forRoot in AppModule
 //  @NgModule({
 //   imports: [
 //     CommonModule
